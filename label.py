0e47d47d1c5f96d67bc2d748b6a076e34ec530ff
Andy Cate <and164v@gmail.com>
2019-05-28 10:13:20 -0700

    Add graph and clean up label.py

--- label.py
+++ label.py
@@ -9,20 +9,26 @@ def bbox_round(results):
             results["detections"][i]["bbox"][j] = int(results["detections"][i]["bbox"][j])
     return results
 
-cap = cv2.VideoCapture(sys.argv[1])
-out = cv2.VideoWriter(sys.argv[2], cv2.VideoWriter_fourcc('m','p','4','v'), cap.get(cv2.CAP_PROP_FPS), (round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-inferrer = TensorFlowInferrer(sys.argv[3])
-postprocessor = DrawingPostprocessor
+def main():
+    cap = cv2.VideoCapture(sys.argv[1])
+    out = cv2.VideoWriter(sys.argv[2], cv2.VideoWriter_fourcc('m','p','4','v'), cap.get(cv2.CAP_PROP_FPS), (round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+    inferrer = TensorFlowInferrer(sys.argv[3])
+    postprocessor = DrawingPostprocessor
+    
+    while(cap.isOpened()):
+        print(f"starting frame {cap.get(cv2.CAP_PROP_POS_FRAMES)} of {cap.get(cv2.CAP_PROP_FRAME_COUNT)}")
+        ret, frame = cap.read()
+        if not ret:
+            break
+        results = inferrer.run(frame)
+        results = bbox_round(results)
+        output = postprocessor.run(None, frame, results)
+        out.write(output)
 
-while(cap.isOpened()):
-    print(f"starting frame {cap.get(cv2.CAP_PROP_POS_FRAMES)} of {cap.get(cv2.CAP_PROP_FRAME_COUNT)}")
-    ret, frame = cap.read()
-    results = inferrer.run(frame)
-    results = bbox_round(results)
-    if len(results["detections"]) > 0:
-        print(f"detections: {len(results['detections'])}")
-    output = postprocessor.run(None, frame, results)
-    out.write(output)
+    cap.release()
 
-cap.release()
-print("done!")
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print('usage: python3 label.py /path/to/input.mp4 /path/to/output.mp4 /path/to/model.pb')
+    main()
+
